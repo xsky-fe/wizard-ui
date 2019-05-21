0798a7dc3687060d690daac4b7b351ec874e34cf
kailang <wangkailang992@gmail.com>
2019-05-21 18:45:08 +0800

    feat: Component playground

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `components`,
+        path: `${__dirname}/src/pages/components`,
       },
     },
     `gatsby-transformer-sharp`,
--- docs/src/components/layout.js
+++ docs/src/components/layout.js
@@ -6,38 +6,68 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
-import { MDXProvider } from '@mdx-js/tag';
+import { StaticQuery, graphql } from "gatsby"
+import { MDXProvider } from '@mdx-js/react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import TopBar from "./TopBar";
 import SideNav from './SideNav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Layout(props) {
-  const { location, children } = props;
-  const { pathname } = location || {};
-  const isComponenet = pathname && (pathname.includes('components') || pathname.includes('layout'));
+// eslint-disable-next-line jsx-a11y/heading-has-content
+const MyH2 = props => <h2 style={{ color: 'red' }} {...props} />
+const MyCodeComponent = ({ children, ...props }) => {
   return (
-    <div>
-      {<TopBar />}
-      <Grid fluid className="Main">
-        {isComponenet ? (
-          <Row>
-            <Col xs={2} md={3}>
-              <SideNav location={location}/>
-            </Col>
-            <Col xs={10} md={9}>
-              <MDXProvider>{children}</MDXProvider>
-            </Col>
-          </Row>
-        ) : <MDXProvider>{children}</MDXProvider>}
-      </Grid>
-    </div>
+    <LiveProvider code={children} >
+      <LiveEditor />
+      <LiveError />
+      <LivePreview />
+    </LiveProvider>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+
+const Layout = (props) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMdx {
+          edges {
+            node {
+              frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={
+      data => {
+        const { location, children } = props;
+        const { pathname } = location || {};
+        const isComponenet = pathname && (pathname.includes('components') || pathname.includes('layout'));
+        return (
+          <div>
+            {<TopBar />}
+            <Grid fluid className="Main">
+              {isComponenet ? (
+                <Row>
+                  <Col xs={2} md={3}>
+                    <SideNav location={location}/>
+                  </Col>
+                  <Col xs={10} md={9}>
+                    <MDXProvider components={{h2: MyH2, code: MyCodeComponent}}>{children}</MDXProvider>
+                  </Col>
+                </Row>
+              ) : <MDXProvider>{children}</MDXProvider>}
+            </Grid>
+          </div>
+        )
+      }
+    }
+  />
+)
 
 export default Layout
