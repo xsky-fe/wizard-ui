4b34a3ae0f5b4fa4b294e5c4130dbb0871a4f793
kailang <wangkailang992@gmail.com>
2019-05-28 14:49:18 +0800

    feat: Add react docgen

--- docs/src/components/Toc/index.js
+++ docs/src/components/Toc/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './style.scss';
+import headTran from '../../utils/head-tran';
 
 function Toc({ headings, location }) {
   return (
     <div className="Main__Toc row">
       {headings.map(({ depth, value }, idx) => {
-        const isCurrent = location.hash && decodeURIComponent(location.hash).includes(value);
+        const key = headTran(value);
+        const isCurrent = location.hash && location.hash.includes(key);
         return (
           <div key={idx} className={`Main__Toc--li${depth}`}>
-            <a href={`#${value}`} className={`${isCurrent ? 'current' : ''}`}>
+            <a href={`#${key}`} className={`${isCurrent ? 'current' : ''}`}>
               {value}
             </a>
           </div>
--- src/lib/Badge/index.tsx
+++ src/lib/Badge/index.tsx
@@ -1,43 +1,9 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import { BadgeProps } from '../../interface';
 import './style.scss';
 
-interface Props {
-  /**
-   * 数值 number|string
-   **/
-  count?: number | string
-  /**
-   * 数值为0时，是否展示 Badge
-   **/
-  showZero?: boolean
-  /**
-   * 展示一个小点
-   **/
-  dot?: boolean
-  /**
-   * 点的大小，只在dot为true的时候生效 ['small', 'middle', 'large']
-   **/
-  size?: string
-  /**
-   * 设置状态颜色 ['default', 'primary', 'success', 'info', 'critical', 'warning', 'danger']
-   **/
-  status?: string,
-  /**
-   * 封顶数值
-   **/
-  overflowCount?: number,
-  /**
-   * 状态点对应的文本
-   **/
-  text?: string,
-  /**
-   * 鼠标hover在点上展现的文字, 默认为count
-   **/
-  title?: string,
-}
-
-const Badge: React.FC<Props> = props => {
+const Badge: React.FC<BadgeProps> = props => {
   const { count, overflowCount, dot, size, showZero, status, text, title, children } = props;
   const statusCls = `Badge--${status}`;
   const parentCls = children ? 'Badge Badge__wrapper' : 'Badge';
