c8e0f4f3413bd367400ab3869f22476d592e8d9b
kailang <wangkailang992@gmail.com>
2019-06-11 12:37:02 +0800

    update

--- stories/components/Badge/index.js
+++ stories/components/Badge/index.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line import/no-webpack-loader-syntax
 import Badge from '!!raw-loader!../../../src/components/Badge';
 import Default from './Default';
+// eslint-disable-next-line import/no-webpack-loader-syntax
 import DefaultCode from '!!raw-loader!./Default';
-
 
 export default {
   default: {
