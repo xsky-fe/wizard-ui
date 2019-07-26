60e40dc235519808e15353efca9705c60364f148
王开朗 <wangkailang992@gmail.com>
2019-07-26 15:48:52 +0800

    fix: Storybook dev

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,14 +1,12 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withOptions } from '@storybook/addon-options';
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
-import './style.scss';
 
 addDecorator (
   withOptions ({
     name: 'wizard ui',
-    url: 'http://github.xsky.com/front-end/wizard',
+    url: 'https://github.com/xsky-fe/wizard-ui',
     sidebarAnimations: true,
-    sortStoriesByKind: true,
     // stories 根据字母，数组小到大排序，根据执行顺序排序
     sortStoriesByKind: true,
     showAddonPanel: false,
