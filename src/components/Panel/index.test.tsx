b449e881e4f5d5d0c57c72c28d6d395e273d3090
侯胜 <hou993268@163.com>
2019-08-14 10:05:30 +0800

    change test case

--- src/components/Panel/index.test.tsx
+++ src/components/Panel/index.test.tsx
@@ -5,7 +5,7 @@ import { shallow } from 'enzyme';
 describe('Panel', () => {
   it('panel showing primary bg', () => {
     const panel = shallow(<Panel bg="primary" />);
-    expect(panel.find('.primary').length).toBe(1);
+    expect(panel.find('.panel-primary-bg').length).toBe(1);
   });
   it('panel showing danger color', () => {
     const panel = shallow(<Panel text="danger" />);
