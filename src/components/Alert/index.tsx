a2fc892e98fafebe6868c1f37f717ee4287b45d7
yukun <yukun@xsky.com>
2019-08-23 16:04:54 +0800

    format document & normalize

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,56 +1,62 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Alert as BSAlert } from 'react-bootstrap';
-import { Icon } from "../";
+import Icon from '../Icon';
 import { AlertProps } from '../../interface';
 import './style.scss';
 
 const icons = {
-  success: "check-circle",
-  warning: "warning",
-  info: "warning",
-  danger: "times-circle",
-}
+  success: 'check-circle',
+  warning: 'warning',
+  info: 'warning',
+  danger: 'times-circle',
+};
 
 const Alert: React.FC<AlertProps> = props => {
   const { children, bsStyle, show, showIcon, onDismiss, dismissDirection } = props;
   const [classes, setClasses] = useState([
-    show ? 'show-alert': 'hidden-alert',
-    showIcon ? 'with-icon': '',
+    show ? 'show-alert' : 'hidden-alert',
+    showIcon ? 'with-icon' : '',
     dismissDirection ? 'dismiss-' + dismissDirection : '',
   ]);
 
   function onClose() {
-    const index = classes.indexOf("show-alert");
+    const index = classes.indexOf('show-alert');
     const tempClass = classes;
-    tempClass[index] = "hidden-alert";
-    setClasses([...tempClass])
+    tempClass[index] = 'hidden-alert';
+    setClasses([...tempClass]);
     onDismiss && onDismiss();
     setTimeout(() => {
-      setClasses([...tempClass, 'hidden'])
+      setClasses([...tempClass, 'hidden']);
     }, 500);
   }
-  
-  return(
+
+  return (
     <BSAlert bsStyle={bsStyle} className={classes.join(' ')} onDismiss={onClose}>
-      {/* 显示前面的图标？*/}
-      {showIcon ? <Icon type={icons[
-        bsStyle ? bsStyle : 'info'
-      ]} /> : null}
+      {showIcon && <Icon type={icons[bsStyle ? bsStyle : 'info']} />}
       {children}
     </BSAlert>
-  )
-}
+  );
+};
 
 Alert.propTypes = {
-  /* 内容 */
+  /* 配置alert内容 */
   children: PropTypes.node,
-  /* 样式 */  
-  bsStyle: PropTypes.oneOf(["success", "warning", "danger", "info"]),
+  /* 配置alert样式（主题） */
+
+  bsStyle: PropTypes.oneOf(['success', 'warning', 'danger', 'info']),
+  /* 配置alert是否显示 */
+
   show: PropTypes.bool,
+  /* 配置是否显示图标 */
+
   showIcon: PropTypes.bool,
+  /* 配置关闭alert时触发的事件 */
+
   onDismiss: PropTypes.func,
-  dismissDirection: PropTypes.oneOf(["up", "right", "down", "left"]),
+  /* 配置alert关闭时消失方向 */
+
+  dismissDirection: PropTypes.oneOf(['up', 'right', 'down', 'left']),
 };
 Alert.defaultProps = {
   bsStyle: 'info',
