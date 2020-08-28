aa1ae1afbb923167999006aa3b8950350418ad58
kailang <kailang@xsky.com>
2020-08-28 19:24:02 +0800

    refactor: update version

--- src/components/VirtualList/index.tsx
+++ src/components/VirtualList/index.tsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import sum from 'lodash/sum';
 import isFunction from 'lodash/isFunction';
-import isEqual from 'lodash/isEqual';
 import {
   VirtualListState,
   VirtualAnchorItem,
