f42de7a7b262f6489afdb8c97a7a40049588a994
fanmengwen <70199559+fanmengwen@users.noreply.github.com>
2023-04-11 17:09:28 +0800

    style: change alert and border radius and box shadow (#212)
    Co-authored-by: fan-mengwen <fan.mengwen@xsky.com>

--- stories/Alert.stories.tsx
+++ stories/Alert.stories.tsx
@@ -3,23 +3,34 @@ import { storiesOf } from '@storybook/react';
 import { Alert } from '../src';
 
 storiesOf('Alert', module)
-.add('danger', () => (
-  <Alert bsStyle="danger" dismissDirection="up">
-    default text <a href="#">test link</a>
-  </Alert>
-))
-.add('info', () => (
-  <Alert bsStyle="info" showIcon dismissDirection="right">
-      default text <a href="#">test link</a>
-  </Alert>
-))
-.add('warning', () => (
-  <Alert bsStyle="warning" dismissDirection="down">
-      default text <a href="#">test link</a>
-  </Alert>
-))
-.add('success', () => (
-  <Alert bsStyle="success" showIcon dismissDirection="left" onDismiss={()=>alert("You can pass a `show` props to the component, and set it to `false` when click the close icon")}>
-      default text <a href="#">test link</a>
-  </Alert>
-))
+  .add('error', () => (
+    <div>
+      <h3>对齐方式</h3>
+      <p>居左对齐</p>
+      <Alert bsStyle="error"  showIcon>
+      这里是 error 级别
+      </Alert>
+      <p>居中对齐</p>
+      <Alert bsStyle="error"  textAlign='center' showIcon>
+      这里是 error 级别
+      </Alert>
+      <p>内置提示</p>
+      <Alert bsStyle="error"  textAlign='inline' showIcon>
+      这里是 error 级别
+      </Alert>
+      <h3>关闭功能</h3>
+      <Alert bsStyle="error" showIcon dismissDirection="right" onDismiss={() =>{}}>
+      这里是 error 级别 <a href="#">点击操作</a>
+      </Alert>
+    </div>
+  ))
+  .add('info', () => (
+    <Alert bsStyle="info" showIcon>
+      这里是 info 级别 <a href="#">点击操作</a>
+    </Alert>
+  ))
+  .add('warning', () => (
+    <Alert bsStyle="warning">
+      这里是 warning 级别 <a href="#">点击操作</a>
+    </Alert>
+  ));
