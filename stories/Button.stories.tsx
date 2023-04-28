941a7fc583e76e3080e1ab3480fdf048e93341b1
hanchuyou <han.chuyou@xsky.com>
2023-04-28 17:21:53 +0800

    change styles
    Signed-off-by: hanchuyou <han.chuyou@xsky.com>

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '../src';
 
-
 storiesOf('DATA SHOW | Button', module)
+  .add('default', () => {
+    return (
+      <div style={{ margin: '10px' }}>
+        <Button style={{marginLeft:'5px'}} bsStyle="info">button</Button>
+        <Button style={{marginLeft:'5px'}} bsStyle="primary">button</Button>
+        <Button style={{marginLeft:'5px'}} bsStyle="danger">button</Button>
+        <Button style={{marginLeft:'5px'}} bsStyle="success">button</Button>
+        <Button style={{marginLeft:'5px'}} bsStyle="warning">button</Button>
+      </div>
+    );
+  })
   .add('toolTipButton', () => {
     const DisabledTooltip = {
       children: <span>don't allowed to click</span>,
-      placement: "top",
-    }
+      placement: 'top',
+    };
     const NormalTooltip = {
       children: <span>allowed to click</span>,
-      placement: "top",
-    }
+      placement: 'top',
+    };
     return (
       <div style={{ margin: '10px' }}>
         <Button
@@ -38,4 +48,4 @@ storiesOf('DATA SHOW | Button', module)
         </Button>
       </div>
     );
-  })
+  });
