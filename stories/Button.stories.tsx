9965a6da3d1d0bd48c9f11fd261dd30fc4c71b3c
hanchuyou <han.chuyou@xsky.com>
2023-04-28 17:32:59 +0800

    add btn-link
    Signed-off-by: hanchuyou <han.chuyou@xsky.com>

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -11,6 +11,7 @@ storiesOf('DATA SHOW | Button', module)
         <Button style={{marginLeft:'5px'}} bsStyle="danger">button</Button>
         <Button style={{marginLeft:'5px'}} bsStyle="success">button</Button>
         <Button style={{marginLeft:'5px'}} bsStyle="warning">button</Button>
+        <Button style={{marginLeft:'5px'}} bsStyle="link">button</Button>
       </div>
     );
   })
