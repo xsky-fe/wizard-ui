05e9b17d05c254f1120f54c6db32d16616f5c199
hanchuyou <han.chuyou@xsky.com>
2023-05-12 18:20:15 +0800

    refactor: panelgroup
    Signed-off-by: hanchuyou <han.chuyou@xsky.com>

--- stories/Panel.stories.tsx
+++ stories/Panel.stories.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Panel } from '../src';
+import {CardGroup} from 'react-bootstrap'
 
 storiesOf('Panel', module)
   .add('basic', () => (
     <Panel header="Head">
       Content
     </Panel>
-  ))
+  )).add('group', () => (
+    <CardGroup>
+      <Panel header="1" >content1</Panel>
+      <Panel header="2" >content2</Panel>
+      <Panel header="3" >content3</Panel>
+      <Panel header="4" >content4</Panel>
+    </CardGroup>
+  ));
