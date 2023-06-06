caadc08f49f740fce279ed15407a0953a8610797
Teacatkk <307519856@qq.com>
2023-06-06 16:03:05 +0800

    fix panel expend and collapse
    Signed-off-by: Teacatkk <307519856@qq.com>

--- stories/Panel.stories.tsx
+++ stories/Panel.stories.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Panel } from '../src';
-import {CardGroup} from 'react-bootstrap'
+import { CardGroup } from 'react-bootstrap';
 
 storiesOf('Panel', module)
-  .add('basic', () => (
-    <Panel header="Head">
-      Content
-    </Panel>
-  )).add('collapse', () => (
+  .add('basic', () => <Panel header="Head">Content</Panel>)
+  .add('collapse', () => (
     <Panel header="Head" collapsible>
       Content
     </Panel>
-  )).add('group', () => (
+  ))
+  .add('group', () => (
     <CardGroup>
-      <Panel header="1" collapsible>content1</Panel>
-      <Panel header="2" collapsible>content2</Panel>
-      <Panel header="3" collapsible>content3</Panel>
-      <Panel header="4" collapsible>content4</Panel>
+      <Panel header="1" collapsible>
+        content1
+      </Panel>
+      <Panel header="2" collapsible>
+        content2
+      </Panel>
+      <Panel header="3" collapsible>
+        content3
+      </Panel>
+      <Panel header="4" collapsible>
+        content4
+      </Panel>
     </CardGroup>
   ));
