6ed0b9df1539b775496c85bcd7887bcaa6060cff
hanchuyou <han.chuyou@xsky.com>
2023-06-24 23:10:22 +0800

    modify webpack config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    '@storybook/preset-scss',
   ],
   framework: {
     "name": "@storybook/react-webpack5",
@@ -14,50 +13,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: true,
-  },
-  webpackFinal: async (config) => {
-    // Remove the existing css rule
-  config.module.rules = config.module.rules.filter(rule => {
-    if (rule.test && rule.test.toString() === '/\\.css$/') {
-      return false;
-    }
-    return true;
-  });
-
-  config.module.rules.push({
-    test: /\.css$/,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
-        },
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          postcssOptions: {},
-        },
-      },
-    ],
-    include: path.resolve(__dirname, '../'),
-  });
-
-  config.module.rules.push({
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: ['babel-loader'],
-  });
-
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: ['babel-loader', 'awesome-typescript-loader'],
-    include: path.resolve(__dirname, '../'),
-  });
-
-
-    return config;
   },
 };
 export default config;
