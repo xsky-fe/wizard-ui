4c45a827bddc5a2eb135b2af1d1c382edb6b7038
csonchen <cson_chensheng@163.com>
2023-06-27 15:34:33 +0800

    feat: upgrade the rc-tree
    Signed-off-by: csonchen <cson_chensheng@163.com>

--- src/components/Tree/index.tsx
+++ src/components/Tree/index.tsx
@@ -1,53 +1,30 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import RcTree, { TreeNode } from 'rc-tree';
-import { TreeData, TreeProps } from '../../interface';
+import RcTree from 'rc-tree';
+import { TreeProps } from '../../interface';
 import './style.scss';
-import pick from 'lodash/pick';
+import 'rc-tree/assets/index.css';
 
-function renderTreeNodes(data: TreeData[]) {
-  return data.map(item => {
-    const filterProps = pick(item, ['title', 'disabled', 'key']);
-    if (item.children) {
-      // @ts-ignore
-      return <TreeNode {...filterProps}>{renderTreeNodes(item.children)}</TreeNode>;
-    } else {
-      // @ts-ignore
-      return <TreeNode {...filterProps} />;
-    }
-  });
-}
+// function renderTreeNodes(data: TreeData[]) {
+//   return data.map(item => {
+//     const filterProps = pick(item, ['title', 'disabled', 'key']);
+//     if (item.children) {
+//       return <TreeNode {...filterProps}>{renderTreeNodes(item.children)}</TreeNode>;
+//     } else {
+//       return <TreeNode {...filterProps} />;
+//     }
+//   });
+// }
 
 const Tree: React.FC<TreeProps> = props => {
-  const { data, checkable, prefixCls } = props;
+  const { data, checkable = true, prefixCls = 'Tree' } = props;
   return (
-    // @ts-ignore
     <RcTree
       {...props}
+      treeData={data}
+      // prefixCls={prefixCls}
       checkable={checkable ? <span className={`${prefixCls}-checkbox-inner`} /> : checkable}
-    >
-      {renderTreeNodes(data)}
-    </RcTree>
+    />
   );
-};
-
-Tree.propTypes = {
-  /** tree 数据 */
-  data: PropTypes.array.isRequired,
-  /** title 部分是否单独可选 */
-  selectable: PropTypes.bool,
-  /** 是否展示 checkbox 勾选框 */
-  checkable: PropTypes.bool,
-  /** 样式前缀，只读 */
-  prefixCls: PropTypes.string,
-  /** 是否展示收缩图标 */
-  showIcon: PropTypes.bool,
-};
-Tree.defaultProps = {
-  prefixCls: 'Tree',
-  selectable: false,
-  checkable: true,
-  showIcon: false,
 };
 
 export default Tree;
--- stories/Tree.stories.tsx
+++ stories/Tree.stories.tsx
@@ -5,32 +5,38 @@ import { Tree } from '../src';
 storiesOf('DATA SHOW | Tree', module).add('default', () => {
   const treeData = [
     {
-      key: '0-0',
+      key: '0',
       title: 'parent 1',
       children: [
         {
-          key: '0-0-0',
+          key: '0-0',
           title: 'parent 1-1',
-          children: [{ key: '0-0-0-0', title: 'parent 1-1-0' }],
+          children: [
+            {
+              key: '0-0-0',
+              title: 'parent 1-1-0',
+            },
+          ],
         },
         {
-          key: '0-0-1',
+          key: '0-1',
           title: 'parent 1-2',
           children: [
-            { key: '0-0-1-0', title: 'parent 1-2-0', disableCheckbox: true },
-            { key: '0-0-1-1', title: 'parent 1-2-1' },
+            {
+              key: '0-1-0',
+              title: 'parent 1-2-0',
+              disableCheckbox: true,
+            },
+            {
+              key: '0-1-1',
+              title: 'parent 1-2-1',
+            },
           ],
         },
       ],
     },
   ];
 
-  return (
-    <Tree
-      data={treeData}
-      onCheck={keys => {
-        alert('你选中了:\n' + keys.join('\n'));
-      }}
-    />
-  );
+  // @ts-ignore
+  return <Tree data={treeData} onClick={() => console.log('clicked..')} />;
 });
